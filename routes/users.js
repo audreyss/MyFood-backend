d01260170fb41dea8bbc5ee218ae5331d4eb8c1e
Audrey Seignol <aseignol@yahoo.fr>
2025-03-03 14:46:34 +0100

    route signup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User = require('../models/users');
+const { body, validationResult } = require("express-validator");
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
+// validator for sign up
+const validateSignIn = [
+  body('email')
+    .notEmpty()
+    .isEmail().withMessage('Not valid email.')
+    .normalizeEmail(),
+  body('password')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long.')
+    .matches(/^[a-zA-Z0-9!@#$%^&*]+$/).withMessage('Password contains not allowed characters.'),
+  body('username')
+    .notEmpty().withMessage('Empty username.')
+    .escape()
+];
+
+
+router.post('/signup', validateSignIn, (req, res) => {
+  // check errors for validation
+  const result = validationResult(req);
+  if (result.notEmpty()) {
+    return res.json({ result: false, error: result.array() });;
+  }
+
+  // check if another user with same email
+  User.findOne({ email: req.body.email })
+    .then(data => {
+      if (data) {
+        // another user with same email
+        return res.json({ result: false, error: 'User already exists with given email.' });
+      }
+      // create new User with hashed password
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32),
+      });
+
+      // save new User 
+      newUser.save()
+        .then(data => {
+          res.json({ result: true, username: data.username })
+        })
+        .catch(error => res.json({ result: false, error }));
+    })
+    .catch(error => res.json({ result: false, error }))
 });
 
 module.exports = router;
