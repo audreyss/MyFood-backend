b39b2d33539086eb193ade2562d1ee483197f03a
Audrey Seignol <aseignol@yahoo.fr>
2025-03-03 15:14:15 +0100

    add route signIn

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
 // validator for sign up
-const validateSignIn = [
+const validateSignUp = [
   body('email')
     .notEmpty()
     .isEmail().withMessage('Not valid email.')
@@ -21,8 +21,8 @@ const validateSignIn = [
 ];
 
 
-router.post('/signup', validateSignIn, (req, res) => {
-  // check errors for validation
+router.post('/signup', validateSignUp, (req, res) => {
+  // check validation
   const result = validationResult(req);
   if (!result.isEmpty()) {
     return res.json({ result: false, error: result.array() });;
@@ -53,5 +53,36 @@ router.post('/signup', validateSignIn, (req, res) => {
     })
     .catch(error => res.json({ result: false, error }))
 });
+
+const validateSignIn = [
+  body('email')
+    .notEmpty()
+    .isEmail().withMessage('Not valid email.')
+    .normalizeEmail(),
+  body('password')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long.')
+    .matches(/^[a-zA-Z0-9!@#$%^&*]+$/).withMessage('Password contains not allowed characters.'),
+];
+
+router.post('/signin', validateSignIn, (req, res) => {
+  // check validation
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.json({ result: false, error: result.array() });
+  }
+
+  // Check if user with same email exists
+  User.findOne({ email: req.body.email })
+    .then(data => {
+      // check if user found has same password
+      if (data && bcrypt.compareSync(req.body.password, data.password)) {
+        res.json({ result: true, username: data.username, token: data.token })
+      } else {
+        // no user found or wrong password
+        res.json({ result: false, error: 'User not found or wrong password.' })
+      }
+    })
+    .catch(error => res.json({ result: false, error }))
+})
 
 module.exports = router;
