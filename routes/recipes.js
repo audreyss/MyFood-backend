8462ad4d2485380dceffa1002a5a1964cd3d8dc9
Audrey Seignol <aseignol@yahoo.fr>
2025-03-05 14:15:12 +0100

    update search query for research by name

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const Recipe = require('../models/recipes');
 
-// Get all recipes
+// ROUTE GET /RECIPES/ALL: get all recipes
 router.get('/all', (req, res) => {
     Recipe.find()
         .then(data => {
@@ -16,26 +16,40 @@ router.get('/all', (req, res) => {
         .catch(error => res.json({ result: false, error }))
 });
 
-// Get all recipes with restriction
+// ROUTE GET /RECIPES/SEARCH: search recipes with given query parameters
+// example: http://localhost:3000/recipes/search?diets=vegetarian,glutenFree&name=soup
+// query parameter: name and diets
+// diets: vegetarian, pregnant, glutenFree, muscleGain, healthy
 router.get('/search', (req, res) => {
     let options = {};
-    for (let diet in req.query) {
-        options[diet] = req.query[diet] == 'true' ? true : false;
+    // loop on req.query
+    for (let opt in req.query) {
+        const value = req.query[opt];
+
+        if (opt == 'name') {
+            // name query
+            options['name'] = {$regex: new RegExp(value, "i") }
+        } else if (opt == 'diets') {
+            // diets query
+            for (let diet of value.split(',')) {
+                options[diet] = true;
+            }
+        } else { 
+            // unknown query
+            return res.json({result: false, error: "Invalid value for query parameter."})
+        }
     }
+    // get recipes with given options
     Recipe.find(options)
         .then(data => {
-            res.json({ result: true, number: data.length, recipes: data })
+            // keep certains fields of recipe
+            const recipes = data.map((recipe) => ({id: recipe._id, name: recipe.name, healthy: recipe.healthy, vegetarian: recipe.vegetarian, 
+                glutenFree: recipe.glutenFree, pregnant: recipe.pregnant, muscleGain: recipe.muscleGain}))
+            res.json({ result: true, number: recipes.length, recipes })
         })
         .catch(error => res.json({ result: false, error }))
 });
 
 
-// Get all recipes name with the restriction
-router.get('/name', (req, res) => {
-    Recipe.find({ muscleGain: true })
-        .then(data => {
-            res.json(data.map((name) => name.name));
-        })
-});
 
 module.exports = router;
