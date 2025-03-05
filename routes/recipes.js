a9ecbec29a570067e1c356a875a58c4d4030f488
Audrey Seignol <aseignol@yahoo.fr>
2025-03-05 15:35:16 +0100

    route recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -8,7 +8,12 @@ router.get('/all', (req, res) => {
     Recipe.find()
         .then(data => {
             if (data) {
-                res.json({ result: true, number: data.length, recipes: data })
+                // keep certains fields of recipe
+                const recipes = data.map((recipe) => ({
+                    id: recipe._id, name: recipe.name, healthy: recipe.healthy, vegetarian: recipe.vegetarian,
+                    glutenFree: recipe.glutenFree, pregnant: recipe.pregnant, muscleGain: recipe.muscleGain
+                }))
+                res.json({ result: true, number: recipes.length, recipes })
             } else {
                 res.json({ result: false });
             }
@@ -28,23 +33,25 @@ router.get('/search', (req, res) => {
 
         if (opt == 'name') {
             // name query
-            options['name'] = {$regex: new RegExp(value, "i") }
+            options['name'] = { $regex: new RegExp(value, "i") }
         } else if (opt == 'diets') {
             // diets query
             for (let diet of value.split(',')) {
                 options[diet] = true;
             }
-        } else { 
+        } else {
             // unknown query
-            return res.json({result: false, error: "Invalid value for query parameter."})
+            return res.json({ result: false, error: "Invalid value for query parameter." })
         }
     }
     // get recipes with given options
     Recipe.find(options)
         .then(data => {
             // keep certains fields of recipe
-            const recipes = data.map((recipe) => ({id: recipe._id, name: recipe.name, healthy: recipe.healthy, vegetarian: recipe.vegetarian, 
-                glutenFree: recipe.glutenFree, pregnant: recipe.pregnant, muscleGain: recipe.muscleGain}))
+            const recipes = data.map((recipe) => ({
+                id: recipe._id, name: recipe.name, healthy: recipe.healthy, vegetarian: recipe.vegetarian,
+                glutenFree: recipe.glutenFree, pregnant: recipe.pregnant, muscleGain: recipe.muscleGain
+            }))
             res.json({ result: true, number: recipes.length, recipes })
         })
         .catch(error => res.json({ result: false, error }))
@@ -53,7 +60,7 @@ router.get('/search', (req, res) => {
 // ROUTE GET /RECIPES/recipe/:id : get information of recipe with given id
 router.get('/recipe/:id', (req, res) => {
     // get recipe with _id = given id
-    Recipe.find({_id: req.params.id})
+    Recipe.find({ _id: req.params.id })
         .then(data => {
             if (data) {
                 res.json({ result: true, recipe: data })
