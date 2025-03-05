5b1b21e132299bf498cfd6911456a17b3cc5f4e2
Benjamin <benjamin.pommier@live.fr>
2025-03-05 11:11:50 +0100

    reciperoute

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 const Recipe = require('../models/recipes');
 
+// Get all recipes with the restriction
 router.get('/', (req, res) => {
+    Recipe.find({ muscleGain: true})
+    .then(data => {
+        res.json(data);
+    })
+});
+
+
+// Get all recipes name with the restriction
+router.get('/name', (req, res) => {
     Recipe.find({ muscleGain: true})
     .then(data => {
         res.json(data.map((name) => name.name));
