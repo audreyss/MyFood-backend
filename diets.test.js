3f3c7066b00d2e6fdebd73a950718618919d53fc
Sergio Capsule <sergiorobin1307@gmail.com>
2025-03-07 11:55:11 +0100

    Added test TDD  for diets

---
+++ diets.test.js
@@ -0,0 +1,13 @@
+// tests/diets.test.js
+const request = require("supertest");
+const app = require("./app"); // Your main app file
+
+it("GET /diets", async () => {
+  // Send a GET request to the `/diets` route
+  const res = await request(app).get("/diets");
+  //On test d'abord la route si elle est egale a 200 (ok)
+  expect(res.statusCode).toBe(200);
+  //si result est egal a true in ThunderClient
+  //We could also do res.body.diets[0].name("Healthy") to check diet name (ThunderClient)
+  expect(res.body.result).toEqual(true);
+});
